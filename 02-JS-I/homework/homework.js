8efd1df8d7eb43210dd9b2b1a132ec37c9776f64
facupascale <macbook@MacBook-Pro-de-Macbook.local>
2020-09-25 22:23:17 -0300

    finishied work

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -208,7 +208,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "∫nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  var hola = ("Hola " + nombre + "!")
+  var hola = ("Hola " + nombre + "!"  )
   return hola
 }
 obtenerSaludo("Martin")
