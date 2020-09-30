3a1d80df6800d9ca5d16837746e833eac8398932
facupascale <facupascale@gmail.com>
2020-09-30 15:02:07 -0300

    finishied work

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,13 +8,28 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
-
+class Usuario {
+    constructor(opciones) {
+  this.usuario = opciones.usuario;
+  this.nombre = opciones.nombre;
+  this.email = opciones.email;
+  this.password = opciones.password;
+}
+  saludar() {
+    return 'Hola, mi nombre es ' + this.nombre
+  }
+}
+  return Usuario
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+
+  Constructor.prototype.saludar = function() {
+    return 'Hello World!'
+  }
 
 }
 
@@ -25,6 +40,13 @@ function agregarStringInvertida() {
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
 
+  String.prototype.reverse = function() {
+      var invertida = '';
+      for (i = this.length - 1; i>= 0; i--) {
+       invertida = invertida + this.charAt(i)
+     }
+  return invertida
+  }
 }
 
 // No modificar nada debajo de esta línea
